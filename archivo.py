b2d7d1a5f157f54c86c49f35e4d387b0f8aee245
Angel Miguel <angelmireles13120@gmail.com>
2024-03-15 19:58:53 -0400

    agrega hola

--- archivo.py
+++ archivo.py
@@ -1,1 +1,39 @@
-chanchito feliz
+from flask import Flask
+
+app = Flask('__name__')
+
+
+@app.route('/suma/<int:num1>,<int:num2>,<int:num3>,<int:num4>')
+def suma(num1, num2, num3, num4):
+
+        resultado = num1 + num2 + num3 + num4
+        return f'<center><h1>{num1} + {num2} + {num3} + {num4} = {resultado}</h1></center>'
+
+
+@app.route('/resta/<int:num1>,<int:num2>,<int:num3>,<int:num4>')
+def resta(num1, num2, num3, num4):
+   
+        resultado = num1 - num2 - num3 - num4
+        return f'<center><h1>{num1} - {num2} - {num3} - {num4} = {resultado}</h1></center>'
+
+
+@app.route('/division/<int:num1>,<int:num2>,<int:num3>,<int:num4>')
+def division(num1, num2, num3, num4):
+        
+        resultado = num1 / num2 / num3 / num4
+        return f'<center><h1>{num1} / {num2} / {num3} / {num4} = {resultado}</h1></center>'
+    
+
+
+@app.route('/multiplicacion/<int:num1>,<int:num2>,<int:num3>,<int:num4>')
+def multiplicacion(num1, num2, num3, num4):
+
+        resultado = num1 * num2 * num3 * num4
+        return f'<center><h1>{num1} * {num2} * {num3} * {num4} = {resultado}</h1></center>'
+    
+@app.route('/')
+def angel():
+    return '<center><h1>Bienvenido</h1></center>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
